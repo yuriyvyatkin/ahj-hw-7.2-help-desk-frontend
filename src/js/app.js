f45e8b81e7e4acd9650c363d1efcfd4fe1b91a2f
Yuriy Vyatkin <yuriyvyatkin@yandex.ru>
2021-09-18 17:55:19 +1000

    Add correct server url in app.js

--- src/js/app.js
+++ src/js/app.js
@@ -22,8 +22,7 @@ const addBtn = new AddBtn(
 
 addBtn.assignHandler();
 
-// const negotiator = new Negotiator('https://help-desk-backend-2021.herokuapp.com/');
-const negotiator = new Negotiator('http://127.0.0.1:3000/');
+const negotiator = new Negotiator('https://help-desk-backend-2021.herokuapp.com/');
 
 const modal = new Modal(
   modalEl,
